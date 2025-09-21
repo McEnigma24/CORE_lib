3fdcc5c17b754482c112e7f7a7ea758dc34fa328
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-09-21 23:26:47 +0200

    quick

--- _inc/__preprocessor__.h
+++ _inc/__preprocessor__.h
@@ -17,11 +17,19 @@ using namespace std;
 
 #define path_run_time_config "../run_time_config"
 
-#define var(x)   cout << #x << " = " << x << '\n';
-#define varr(x)  cout << #x << " = " << x << ' ';
-#define line(x)  cout << x << '\n';
-#define linee(x) cout << x << ' ';
-#define nline    cout << '\n';
+#define __FILENAME__ ((__FILE__) + SOURCE_PATH_SIZE)
+
+#define var(x)   cout << __FILENAME__ << ":" << __LINE__ << " - " << #x << " = " << x << "\n";
+#define varr(x)  cout << __FILENAME__ << ":" << __LINE__ << " - " << #x << " = " << x << " ";
+#define line(x)  cout << __FILENAME__ << ":" << __LINE__ << " - " << x << "\n";
+#define linee(x) cout << __FILENAME__ << ":" << __LINE__ << " - " << x << " ";
+#define nline    cout << "\n";
+
+// #define var(x)   cout << #x << " = " << x << '\n';
+// #define varr(x)  cout << #x << " = " << x << ' ';
+// #define line(x)  cout << x << '\n';
+// #define linee(x) cout << x << ' ';
+// #define nline    cout << '\n';
 
 typedef uint8_t u8;
 typedef uint16_t u16;
